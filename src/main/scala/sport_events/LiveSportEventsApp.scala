13611193cffdfb29f2f16835e9ea940ff99823a5
unknown <panamavr@hotmail.com>
2022-09-13 17:04:39 +0100

    f

--- src/main/scala/sport_events/LiveSportEventsApp.scala
+++ src/main/scala/sport_events/LiveSportEventsApp.scala
@@ -4,29 +4,32 @@ import com.typesafe.scalalogging.Logger
 import org.slf4j.LoggerFactory
 import sport_events.clients.SportEventsHttpClient
 import sport_events.environment.LiveSportEventsRuntime
-import sport_events.errors.{DomainServiceError, EventsNotStreamedError}
-import sport_events.models.Event
+import sport_events.errors.EventsNotStreamedError
+import sport_events.models.EventsOverview
 import sport_events.services.EventDetailsService
 import sport_events.services.EventDetailsService.{getAllEvents, getLastEvent, getLastEvents}
 import sport_events.validate.LiveEventsValidator.validate
+import zio.Exit.{Failure, Success}
 import zio.{Exit, Has, ZIO}
 
 object LiveSportEventsApp extends App {
 
   type RuntimeEnv = Has[SportEventsHttpClient.Service] with Has[EventDetailsService.Service]
+
   private val logger = Logger(LoggerFactory.getLogger(getClass.getName))
 
-  val runtime: zio.Runtime.Managed[RuntimeEnv] = new LiveSportEventsRuntime().runtime
+  logger.info("Starting service...")
+  val runtime: zio.Runtime[RuntimeEnv] = new LiveSportEventsRuntime().runtime
 
-  val flow: ZIO[RuntimeEnv, DomainServiceError, Tuple3[Seq[Event], Seq[Event], Option[Event]]] = for {
+  val flow = for {
       sportEvents <- ZIO.service[SportEventsHttpClient.Service]
         .flatMap(_.getSportEvents("sample1.txt"))
         .mapError(e => EventsNotStreamedError(e.message))
       validatedEvents <- ZIO.succeed(sportEvents.sliding(2).filter(validate).flatten)
-      allEvents <- getAllEvents(validatedEvents)
-      lastNEvents <- getLastEvents(5, allEvents)
-      lastEvent <- getLastEvent(allEvents)
-  } yield (lastNEvents, allEvents, lastEvent)
-  
-  runtime.unsafeRunAsync(flow)
+  } yield (validatedEvents.toSeq)
+
+  runtime.unsafeRunAsync(flow) {
+    case Success(events) => logger.info(s"Got events")
+    case Failure(e) => logger.error(s"${e.toString}")
+  }
 }
